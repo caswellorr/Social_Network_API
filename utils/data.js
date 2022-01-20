ac390428e3c03c59b64669412bc29a68f02267c2
Caswell Orr <caswell.orr@gmail.com>
2022-01-19 19:03:05 -0600

    seed and data - hopefully it is correct. one way to find out..

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,4 @@
-const names = [
+const usernames = [
   'Aaran',
   'Aaren',
   'Aarez',
@@ -74,7 +74,7 @@ const names = [
   'Parker',
 ];
 
-const appDescriptions = [
+const thoughts = [
   'Decision Tracker',
   'Find My Phone',
   'Learn Piano',
@@ -95,24 +95,52 @@ const appDescriptions = [
   'Deliveries',
 ];
 
+const reactions = [
+  'Decision Trackers are awesome',
+  'Find My Phone is a useful app',
+  'Learn Piano is not very good for learning Piano',
+  'Starbase Defender is a great game, I love it',
+  'Tower Defense is okay',
+  'Monopoly Money is better than real money IMO',
+  'Movie trailers are just the best parts of a movie distilled into 90 seconds',
+  'Hello world, this is a comment',
+  'Social media is a big waste of time',
+  'Notes is my most used app',
+  'Messages is open on my computer 24/7',
+  'Email is open on my computer',
+  'Compass is never opened',
+  'Firefox is great for privacy',
+];
+
 // Get a random item given an array
 const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-// Gets a random full name
-const getRandomName = () =>
-  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+// Gets a random username
+const getRandomUsername = () =>
+  `${getRandomArrItem(usernames)}`;
 
-// Function to generate random assignments that we can add to student object.
-const getRandomAssignments = (int) => {
+// Function to generate random thoughts that we can add to user object.
+const getRandomThoughts = (int) => {
   const results = [];
   for (let i = 0; i < int; i++) {
     results.push({
-      assignmentName: getRandomArrItem(appDescriptions),
-      score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
+      thoughtText: getRandomArrItem(thoughts)
+    });
+  }
+  return results;
+};
+  
+
+// Function to generate random reactions that we can add to thought object.
+const getRandomReactions = (int) => {
+  const results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      reactionBody: getRandomArrItem(reactions)
     });
   }
   return results;
 };
 
 // Export the functions for use in seed.js
-module.exports = { getRandomName, getRandomAssignments };
+module.exports = { getRandomUsername, getRandomThoughts, getRandomReactions };
